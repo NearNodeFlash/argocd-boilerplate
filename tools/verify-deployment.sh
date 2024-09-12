d5035861fe24bf17260f3f93f20ef30feed93645
Dean Roehrich <dean.roehrich@hpe.com>
2024-09-12 14:55:22 -0500

    Verify that site-specific yamls reference latest API versions (#34)
    Signed-off-by: Dean Roehrich <dean.roehrich@hpe.com>

--- tools/verify-deployment.sh
+++ tools/verify-deployment.sh
@@ -44,6 +44,9 @@ elif [[ ! -d environments/$ENV ]]; then
 fi
 
 LC_ALL=C
+TOC="environments/$ENV/manifest-toc.txt"
+API_VERSION_FILES=
+FILES_NEEDING_APIVER=
 
 set -e
 
@@ -57,6 +60,36 @@ kustomize_build() {
     else
         bin/kustomize build "$path" > /dev/null || exit 1
     fi
+}
+
+check_use_of_non_hub_api_versions() {
+    # Look for any references to an old API, but skip the files that came
+    # from the tarball manifest or that are in the reference/ subdir, which
+    # is owned by unpack-manifest.
+
+    if ! API_VERSION_FILES=$(find environments/"$ENV" -name api-version.txt); then
+        return
+    fi
+    if [[ ! -f $TOC ]]; then
+        echo "Unable to find the manifest table of contents."
+        exit 1
+    fi
+    for apiver_file in $API_VERSION_FILES; do
+        apiversion=$(<"$apiver_file")
+        apigroup=$(dirname "$apiversion")
+        if fnames=$(grep -lr "$apigroup/" environments/"$ENV"); then
+            for fname in $fnames; do
+                # The reference dir is owned by unpack-manifest.
+                [[ $fname == environments/"$ENV"/*/reference/* ]] && continue
+                # Skip files that come from the manifest tarball.
+                grep -q "$fname" "$TOC" && continue
+
+                if ! grep -q -E "^apiVersion: $apiversion$" "$fname"; then
+                    FILES_NEEDING_APIVER="$FILES_NEEDING_APIVER $fname"
+                fi
+            done
+        fi
+    done
 }
 
 for bootstrap in environments/"$ENV"/*bootstrap*
@@ -73,3 +106,22 @@ do
         kustomize_build "$path"
     done
 done
+
+check_use_of_non_hub_api_versions
+if [[ -n $FILES_NEEDING_APIVER ]]; then
+    echo
+    echo "Update these files to ensure their 'apiVersion' value is pointing at"
+    echo "the latest API version:"
+    for fname in $FILES_NEEDING_APIVER; do
+        echo "  $fname"
+    done
+    echo
+    if [[ -n $API_VERSION_FILES ]]; then
+        echo "  The 'apiVersion' values should be updated to one of the following:"
+        for fname in $API_VERSION_FILES; do
+            echo "    $(<"$fname")"
+        done
+        echo
+    fi
+fi
+
